0757c6c798fd3f333d693b27c58d708e4a39d9d8
juhahinkula <hinkula@gmail.com>
2018-11-14 09:30:07 +0200

    initial commit

---
+++ src/main/java/fi/haagahelia/servercourse/ServercourseApplication.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.servercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServercourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServercourseApplication.class, args);
+	}
+}
